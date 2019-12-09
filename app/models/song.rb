635e6273ed3be3e2d195f6ddb67b80c6c451951f
Matthew C <m23.cullen@gmail.com>
2019-12-09 09:45:59 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist 
   belongs_to :genre 
   has_many :notes
-  accepts_nested_attributes_for :notes
+  accepts_nested_attributes_for :notes, :reject_if => proc { |attrs| attrs[:content].blank? }
    
   def artist_name=(artist_name)
     self.artist = Artist.find_or_create_by(name: artist_name)
