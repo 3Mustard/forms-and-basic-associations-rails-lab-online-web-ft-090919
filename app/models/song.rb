8e6693ce0df73f9daa14f20cd17616d9d98eb8fa
Matthew C <m23.cullen@gmail.com>
2019-12-09 09:42:21 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,8 @@ class Song < ActiveRecord::Base
   belongs_to :artist 
   belongs_to :genre 
   has_many :notes
-  
+  accepts_nested_attributes_for :notes
+   
   def artist_name=(artist_name)
     self.artist = Artist.find_or_create_by(name: artist_name)
   end
@@ -17,21 +18,5 @@ class Song < ActiveRecord::Base
   
   def genre_name
     self.try(:genre).try(:name)
-  end 
-  
-  def note_contents=(note_contents)
-    note_contents.each do |note|
-      self.notes << Note.find_or_create_by(content: note)
-    end 
-  end 
-  
-  def note_contents
-    note_array = []
-    self.notes.each do |note|
-      if note.content != ""
-        note_array << note.content
-      end
-      note_array.uniq
-    end 
   end 
 end
